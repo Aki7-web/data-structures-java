c9154c24ba5a1803b3f8def9a64c9580fc68a0b5
Suhani Singh <akisingh2006.in@gmail.com>
2026-05-20 13:19:14 +0530

    max stock profit value returns

---
+++ Arrays/buyandsellstocks.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class buyandsellstocks {
+    public static int stocks(int stock[]){
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for(int i=0; i<stock.length;i++){
+            if(buyPrice<stock[i]){
+                int profit= stock[i]-buyPrice;
+                maxProfit=Math.max(profit, maxProfit);
+            }else{
+                buyPrice=stock[i];
+            }
+        }
+
+        return maxProfit;
+ 
+    }
+
+    public static void main(String args[]){
+        int stock[]={7,1,5,3,6,4};
+        System.out.println(stocks(stock));
+    }
+
+}
