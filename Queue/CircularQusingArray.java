2ef8a8779e53dac1e7dd23fd87ce0d67a1f46712
Aki7-web <akisingh2006.in@gmail.com>
2026-08-01 15:08:34 +0530

    circular queue using array

--- Queue/CircularQusingArray.java
+++ Queue/CircularQusingArray.java
@@ -1,35 +1,45 @@
 package Queue;
 
-public class QueueArray {
+public class CircularQusingArray {
 
     static class Queue{
         static int arr[];
         static int size;
         static int rear;
+        static int front;
+
         Queue(int n){
             arr= new int[n];
             rear=-1;
             size=n;
+            front=-1;
         }
 
         //is empty condition
         public static boolean isEmpty(){
-            return rear==-1;
+            return (rear==-1 && front==-1);
+        }
+
+        //is the queue full
+        public static boolean isFull(){
+            return (rear+1)%size==front;
         }
 
         //add condition O(1) ENQUEUE
         public static void add(int data){
-            if(rear==size-1){
+            if((rear+1)%size==front){
                 System.out.println("Queue is full");
                 return;
             }
             if(isEmpty()){
-                arr[0]=data;
-                rear= rear+1;
+                rear= front=0;
+                arr[front]=data;
                 return;
             }
-            arr[rear+1]=data;
-            rear=rear+1;
+
+            rear=(rear+1)%size;
+            arr[rear]=data;
+            
             return;
 
         }
@@ -40,17 +50,23 @@ public class QueueArray {
                 System.out.println("queue is empty");
                 return -1;
             }
-            int front = arr[0];
-            for(int i=0;i<arr.length-1;i++){
-                arr[i]=arr[i+1];
+
+           
+            int result = arr[front];
+             //last element delete
+            if(rear==front){
+                rear=front=-1;
+            }else{
+                front=(front+1)%size;
             }
-            rear=rear-1;
-            return front;
+            
+            
+            return result;
         }
 
         //peak fxtn O(1)
         public static int peek(){
-            return arr[0];
+            return arr[front];
         }
     }
     public static void main(String[] args) {
@@ -58,6 +74,10 @@ public class QueueArray {
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.pop());
+        q.add(4);
+        System.out.println(q.pop());
+        q.add(5);
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
